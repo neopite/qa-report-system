34d9305efcda97b31197ed48144fd1069f92489d
Yaroslav <Stamin4ik4@gmail.com>
2021-05-13 09:29:45 +0300

    changes

---
+++ src/test/java/api/LoginTest.java
@@ -0,0 +1,46 @@
+package api;
+
+import entities.Error;
+import entities.Login;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SerenityRunner.class)
+public class LoginTest {
+
+    @Test
+    public void verifyLoginExistedUserAndReturn200(){
+        Login login = new Login();
+        login.setEmail("george.bluth@reqres.in");
+        login.setPassword("123456");
+        new ApiEndpoints().loginUser(login).then().statusCode(200);
+    }
+
+    @Test
+    public void verifyLoginNonExistedUserAndReturn400(){
+        Login login = new Login();
+        login.setEmail("NotExisted@gmail.com");
+        login.setPassword("123456");
+        new ApiEndpoints().loginUser(login).then().statusCode(400);
+    }
+
+    @Test
+    public void verifyLoginNonExistedUserAndReceiveError(){
+        Login login = new Login();
+        login.setEmail("NotExisted@gmail.com");
+        login.setPassword("123456");
+        Error error = new ApiEndpoints().loginUser(login).as(Error.class);
+        assertEquals("user not found",error.getError());
+    }
+
+    @Test
+    public void verifyLoginWithEmptyPassword(){
+        Login login = new Login();
+        login.setEmail("eve.holt@reqres.in");
+        Error error = new ApiEndpoints().loginUser(login).as(Error.class);
+        assertEquals("Missing password",error.getError());
+    }
+}
--- src/test/java/ui/LogoutTest.java
+++ src/test/java/ui/LogoutTest.java
@@ -12,23 +12,23 @@ import org.openqa.selenium.WebDriver;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-@RunWith(SerenityRunner.class)
-public class LogoutTest extends BasicTestSetup {
-
-    @Managed
-    WebDriver webDriver;
-
-    @Steps
-    public LogoutSteps steps;
-
-    @Test
-    @Screenshots(beforeAndAfterEachStep = true)
-    public void successfulLogout(){
-        ThucydidesWebDriverSupport.useDriver(webDriver);
-        steps.openSite();
-        steps.loginSite();
-        steps.openLoginMenu();
-        steps.logout();
-        steps.check(); 
-    }             
-}
+//@RunWith(SerenityRunner.class)
+//public class LogoutTest extends BasicTestSetup {
+//
+//    @Managed
+//    WebDriver webDriver;
+//
+//    @Steps
+//    public LogoutSteps steps;
+//
+//    @Test
+//    @Screenshots(beforeAndAfterEachStep = true)
+//    public void successfulLogout(){
+//        ThucydidesWebDriverSupport.useDriver(webDriver);
+//        steps.openSite();
+//        steps.loginSite();
+//        steps.openLoginMenu();
+//        steps.logout();
+//        steps.check();
+//    }
+//}
